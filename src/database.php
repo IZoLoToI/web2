c6f1db35a6d642887994646d29b8211adf6478ee
IZoLoToI <104593248+IZoLoToI@users.noreply.github.com>
2023-03-01 14:50:26 +0300

    Add files via upload

---
+++ src/database.php
@@ -0,0 +1,34 @@
+<?php
+
+
+require '../vendor/autoload.php';
+
+class Database
+{
+    private string $host = 'localhost';
+    private string $db_name = 'shahovDB';
+    private string $username = 'root';
+    private string $password = 'tytinakoliana3459';
+
+    public ?PDO $conn = null;
+
+    public function getConnection(): ?PDO
+    {
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+            echo $_ENV['MYSQL_HOST'];
+            
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+
+}
+
+
+
+
