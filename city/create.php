5b11ad888b809d6cbeef010f144ad9f7c50294d0
IZoLoToI <104593248+IZoLoToI@users.noreply.github.com>
2023-03-08 23:38:40 +0300

    Update create.php

--- city/create.php
+++ city/create.php
@@ -3,37 +3,28 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 
-
 include_once '../src/database.php';
 include_once '../models/city.php';
 
-$DB = new Database();
-$DBGet = $DB->getConnection();
+$db = new Database();
+$dbGet = $DB->getConnection();
 
-$City = new City($DBGet);
-
-
+$сity = new Сity($DBGet);
 
 if (
     !empty($_POST['name']) 
 ) {
 
+    $city->name = $_POST['name'];
 
-    $City->name = $_POST['name'];
-
-
-
-    if ($City->create()) {
-
+    if ($city->create()) {
         http_response_code(201);
         echo json_encode(array("message" => "Город был создан."));
     } else {
         http_response_code(503);
-
         echo json_encode(["message" => "Невозможно создать город."]);
     }
 } else {
-
     http_response_code(400);
     echo json_encode(["message" => "Невозможно создать город. Данные неполные."]);
 }
