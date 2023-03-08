e4af18ea6a024570482a4f4f61273a82ae12c4ee
IZoLoToI <104593248+IZoLoToI@users.noreply.github.com>
2023-03-08 23:46:50 +0300

    Update create.php

--- user/create.php
+++ user/create.php
@@ -3,16 +3,13 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 
-
 include_once '../src/database.php';
 include_once '../models/user.php';
 
-$DB = new Database();
-$DBC = $DB->getConnection();
+$db = new Database();
+$dbC = $db->getConnection();
 
-$User = new User($DBC);
-
-
+$user = new User($dbC);
 
 if (
     !empty($_POST['idCity']) &&
@@ -20,25 +17,19 @@ if (
     !empty($_POST['username'])
 ) {
 
+    $user->name = $_POST['name'];
+    $user->idCity = $_POST['idCity'];
+    $user->username = $_POST['username'];
 
-    $User->name = $_POST['name'];
-    $User->idCity = $_POST['idCity'];
-    $User->username = $_POST['username'];
-
-
-
-    if ($User->create()) {
+    if ($user->create()) {
 
         http_response_code(201);
         echo json_encode(array("message" => "Пользователь был создан."));
     } else {
         http_response_code(503);
-
         echo json_encode(["message" => "Невозможно создать пользователя."]);
     }
 } else {
-
     http_response_code(400);
-
     echo json_encode(["message" => "Невозможно создать пользователя. Данные неполные."]);
 }
