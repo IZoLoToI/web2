df88e1e6197249af4fe74674d3b87bc6a18c0215
IZoLoToI <104593248+IZoLoToI@users.noreply.github.com>
2023-03-08 23:44:47 +0300

    Update update.php

--- city/update.php
+++ city/update.php
@@ -5,37 +5,31 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-
-
 include_once '../src/database.php';
 include_once '../models/city.php';
 
-$DB = new Database();
-$DBC = $DB->getConnection();
+$db = new Database();
+$dbC = $db->getConnection();
 
-$City = new City($DBC);
-
-
+$city = new City($DBC);
 
 if (
     !empty($_POST['id']) &&
     !empty($_POST['name']) 
 ) {
 
-    $City->id  = $_POST['id'];
-    $City->name = $_POST['name'];
+    $city->id  = $_POST['id'];
+    $city->name = $_POST['name'];
   
-    if ($City->update()) {
+    if ($city->update()) {
 
         http_response_code(201);
         echo json_encode(array("message" => "Город был обновлен."));
     } else {
         http_response_code(503);
-
         echo json_encode(["message" => "Невозможно обновить город."]);
     }
 }else{
     http_response_code(400);
-
     echo json_encode(["message" => "Невозможно обновить город. Данные неполные."]);
 }
